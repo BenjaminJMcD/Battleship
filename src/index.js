d1616583ca755eb495c9a41ca02575dd71dfa5b8
BenjaminJMcD <BenjaminJMcD@outlook.com>
2024-04-13 14:32:03 -0500

    added index.js before merging

--- src/index.js
+++ src/index.js
@@ -2,19 +2,49 @@ import './style.css';
 import Ship from './Ship';
 import Gameboard from './Gameboard';
 
-let ship5 = new Ship(5);
+
 
 let playerBoard = new Gameboard;
 let computerBoard = new Gameboard;
 
+playerBoard.ships.ship5.vert = true;
 
-console.log(playerBoard.board);
-console.log(computerBoard.board);
 
-playerBoard.placeShip(ship5, 4, 4);
+playerBoard.placeShip(playerBoard.ships.ship5, 4, 4);
+playerBoard.placeShip(playerBoard.ships.ship4, 0, 0);
+playerBoard.placeShip(playerBoard.ships.ship3, 1, 0);
+playerBoard.placeShip(playerBoard.ships.ship2, 2, 0);
+playerBoard.placeShip(playerBoard.ships.ship1, 3, 0);
 
-console.log(playerBoard.board)
+console.log(playerBoard.ships.ship1.placement)
 
-playerBoard.receiveAttack(4, 5)
 
+console.log(playerBoard.ships.ship5.placement);
+
+console.log(playerBoard.ships.ship1.length)
+
+playerBoard.receiveAttack(4, 4);
+playerBoard.receiveAttack(5,4)
+
+console.log(playerBoard)
+
+playerBoard.receiveAttack(6,4)
+playerBoard.receiveAttack(7,4)
+playerBoard.receiveAttack(0,0)
+playerBoard.receiveAttack(0,1)
+playerBoard.receiveAttack(0,2)
+playerBoard.receiveAttack(0,3)
+playerBoard.receiveAttack(0,4)
+playerBoard.receiveAttack(1,1)
+playerBoard.receiveAttack(1,0)
+playerBoard.receiveAttack(1,2)
+playerBoard.receiveAttack(2,0)
+playerBoard.receiveAttack(2,1)
+playerBoard.receiveAttack(2,2)
+playerBoard.receiveAttack(8,4)
+
+playerBoard.receiveAttack(3,0)
+playerBoard.receiveAttack(3,1)
+
+playerBoard.reportSunk();
 
